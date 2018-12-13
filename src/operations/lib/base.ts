1230328a2ec7ad140df47a74b21d57682af46ef1
Jared Noble <jsnoble1@gmail.com>
2018-12-13 08:33:28 -0700

    fixed base64code issues and op refactor resolves #4

--- src/operations/lib/base.ts
+++ src/operations/lib/base.ts
@@ -3,14 +3,27 @@ import { DataEntity } from '@terascope/job-components';
 import { OperationConfig } from '../../interfaces'
 
 export default abstract class OperationBase {
-    //@ts-ignore
     protected source: string;
+    protected target: string;
+    protected removeSource: boolean;
+
+    constructor(config: OperationConfig) {
+        this.source = '';
+        this.target = '';
+        this.removeSource = config.remove_source || false;
+        this.validate(config);
+    }
 
     protected validate(config: OperationConfig) {
-        const { target_field: field } = config;
-        if (!field || typeof field !== 'string' || field.length === 0) throw new Error(`could not find target_field for ${this.constructor.name} validation or it is improperly formatted, config: ${JSON.stringify(config)}`);
-        const source = this.parseField(field);
-        this.source = source;
+        // we don't need to check target or source for selector ops
+        if (this.constructor.name === 'Selector') return;
+        const { target_field: targetField, source_field: sField, remove_source } = config;
+        let tField = targetField || sField;
+        if (remove_source && typeof remove_source !== 'boolean') throw new Error('remove_source if specified must be of type boolean')
+        if (!tField || typeof tField !== 'string' || tField.length === 0) throw new Error(`could not find target_field for ${this.constructor.name} validation or it is improperly formatted, config: ${JSON.stringify(config)}`);
+        if (!sField || typeof sField !== 'string' || sField.length === 0) throw new Error(`could not find source_field for ${this.constructor.name} validation or it is improperly formatted, config: ${JSON.stringify(config)}`);
+        this.source = this.parseField(sField);
+        this.target = this.parseField(tField);
     }
 
     protected parseField(str: string): string {
